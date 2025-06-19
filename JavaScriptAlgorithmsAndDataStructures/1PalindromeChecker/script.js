3b826bd1e34c56a3040155720be0fd0b6b6898ba
nchinmertz <48361293+nchinmertz@users.noreply.github.com>
2025-06-19 10:30:16 -0400

    Palindrome Checker

---
+++ JavaScriptAlgorithmsAndDataStructures/1PalindromeChecker/script.js
@@ -0,0 +1,29 @@
+const userInput = document.getElementById("text-input")
+const checkBtn = document.getElementById("check-btn")
+const ret = document.getElementById("result")
+
+const checkPalindrome = (input) =>{
+    const originalInput = input;
+    if (input===""){
+        alert("Please input a value");
+        return;
+    }
+    ret.replaceChildren();
+
+    const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
+    let resultMsg = `${originalInput} ${
+      lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'
+    } a palindrome.`;
+
+    const pTag = document.createElement('p');
+    pTag.className = 'user-input';
+    pTag.innerText = resultMsg;
+    ret.appendChild(pTag);
+
+    ret.classList.remove('hidden');
+}
+
+checkBtn.addEventListener("click", () => {
+    checkPalindrome(userInput.value);
+    userInput.value = "";
+})
