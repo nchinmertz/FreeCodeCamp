1853fcc4b108bc5b58f28c535f6c93b39fb60436
nchinmertz <48361293+nchinmertz@users.noreply.github.com>
2025-07-12 15:25:19 -0400

    Create 2create-a-complex-jsx-element.js

---
+++ frontEndDevelopmentLibraries/4React/2create-a-complex-jsx-element.js
@@ -0,0 +1,14 @@
+** start of index.jsx **
+
+const JSX = <div>
+              <h1></h1>
+              <p></p>
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+
+** end of index.jsx **
+
