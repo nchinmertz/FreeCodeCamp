50ffc4b3a1dac9135a38a3b39526399d2a1c2aa7
nchinmertz <48361293+nchinmertz@users.noreply.github.com>
2025-08-03 19:41:40 -0400

    Telephone Number Validator

---
+++ JavaScriptAlgorithmsAndDataStructures/3TelephoneNumberValidator/script.js
@@ -0,0 +1,17 @@
+const userInput = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const result = document.getElementById("results-div");
+
+function checkUserInput(){
+    if (userInput.value === "") {
+        alert("Please provide a phone number");
+        return;
+    }else{
+        const regex = /^(1\s?)?(\([0-9]{3}\)|[0-9]{3})([\s\-]?)([0-9]{3}[\s\-]?[0-9]{4}$)/;
+        result.innerHTML = (regex.test(userInput.value)?"Valid US number: ":"Invalid US number: ")+userInput.value;
+    }
+}
+
+checkBtn.addEventListener("click", checkUserInput);
+clearBtn.addEventListener("click", ()=>{result.innerHTML=""})
